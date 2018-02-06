386f8948a31fc7fbf293c5c4515d8bea1cb178e9
alapshov <lapshov.av@mail.ru>
2018-02-07 00:37:13 +0300

    Refactoring

--- Software-Testing-Courses/MyFirstTest.cs
+++ Software-Testing-Courses/MyFirstTest.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -22,21 +17,21 @@ namespace Software_Testing_Courses
         public void start()
         {
             driver = new ChromeDriver(@"C:\Driver"); //Указать путь к драйверу
-            
-            driver.Url = "https://www.google.ru";
-            driver.FindElement(By.Name("q")).SendKeys("Тестовый поиск");
-            driver.FindElement(By.XPath(@"//*[@id=""sbtc""]/div[2]/div[2]/div[1]/div/ul/li[2]/div/span[1]/span/input")).Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));           
 
         }
         [Test]
         public void FirstTest()
         {
-
+            driver.Url = "https://www.google.ru";
+            driver.FindElement(By.Name("q")).SendKeys("Тестовый поиск");
+            driver.FindElement(By.XPath(@"//*[@id=""sbtc""]/div[2]/div[2]/div[1]/div/ul/li[2]/div/span[1]/span/input")).Click();
         }
         [TearDown]
         public void stop()
         {
-
+            driver.Quit();
+            driver = null;
         }
 
     }
