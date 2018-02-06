29654b8d80663e314fd0e9d4bfe557424edfb536
alapshov <lapshov.av@mail.ru>
2018-02-07 00:31:21 +0300

    MyFirstCommit
    SartSeleniumProject

---
+++ Software-Testing-Courses/MyFirstTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace Software_Testing_Courses
+{
+    [TestFixture]
+    class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+       
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver(@"C:\Driver"); //Указать путь к драйверу
+            
+            driver.Url = "https://www.google.ru";
+            driver.FindElement(By.Name("q")).SendKeys("Тестовый поиск");
+            driver.FindElement(By.XPath(@"//*[@id=""sbtc""]/div[2]/div[2]/div[1]/div/ul/li[2]/div/span[1]/span/input")).Click();
+
+        }
+        [Test]
+        public void FirstTest()
+        {
+
+        }
+        [TearDown]
+        public void stop()
+        {
+
+        }
+
+    }
+}
---
+++ Software-Testing-Courses/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Software-Testing-Courses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Software-Testing-Courses")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f48dc62f-6b71-43e8-9123-4d2de164b4a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
